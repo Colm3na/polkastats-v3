4497999419137a7c1c18a81e28b6abcfed22c0de
mariopino <info@quequiereshacer.es>
2019-10-26 00:49:17 +0200

    Add intentions to store

--- store/intentions.js
+++ store/intentions.js
@@ -6,6 +6,8 @@ export const state = () => ({
   
 export const mutations = {
   update (state, validators) {
+    // Sort validators by total bonded desc
+    validators.sort((a, b) => (a.stakingLedger.total < b.stakingLedger.total) ? 1 : -1)
     state.list = validators;
   },
   getters: function() {
@@ -15,7 +17,7 @@ export const mutations = {
 
 export const actions = {
   update (context) {
-    axios.get('https://polkastats.io:8443/validators')
+    axios.get('https://polkastats.io:8443/intentions')
       .then(function (response) {
         context.commit('update', response.data);
       })
