d6dac8dd2fce83ac539504e788d302146425d1a4
mariopino <info@quequiereshacer.es>
2019-10-25 19:19:49 +0200

    Debug

--- pages/index.vue
+++ pages/index.vue
@@ -385,10 +385,10 @@ export default {
     // Use here this.isHex function
     formatNumber(n) {
       if (this.isHex(n)) {
-        //console.log(n + ' is HEX');
+        console.log(n + ' is HEX');
         return (parseInt(n, 16).toString()).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
       } else {
-        //console.log(n + ' is NOT HEX');
+        console.log(n + ' is NOT HEX');
         return (n.toString()).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
       }
     },
