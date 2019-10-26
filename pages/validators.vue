c78673dc375cebb2dfd140e58615d98196990160
mariopino <info@quequiereshacer.es>
2019-10-26 21:20:10 +0200

    Improve validators page

--- pages/validators.vue
+++ pages/validators.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <section>
-      <b-container class="main pt-4 pb-5">
+      <b-container class="main pt-4 pb-5 text-center">
         <h1 class="mb-4">VALIDATORS</h1>
 
         <p>You own a validator and want to have your name, logo, website url and social links included in PolkaStats?</p>
@@ -10,18 +10,36 @@
 
         <h2>Include your validator info in PolkaStats</h2>
 
-        <p>NOTE: You will need to register in https://keybase.io/ and fill all your data in order to continue.</p>
+        <p>NOTE: You will need to register in <a href="https://keybase.io/" target="_blank">https://keybase.io/</a> and fill all your data in order to continue.</p>
 
-        <ul>
-          <li>1. Go to PolkaStats v2 GitHub repo at <a href="https://github.com/Colm3na/polkastats-v2" target="_blank">https://github.com/Colm3na/polkastats-v2</a></li>
-          <li>2. Make a Fork of PolkaStats v2.</li>
-          <li>3. Create on it a new file in identities folder named like stash_account_of_your_validator.json. Example: ET9SkhNZhY7KT474vkCEJtAjbgJdaqAGW4beeeUJyDQ3SnA.json</li>
-          <li>4. The content of the file should be your username in https://keybase.io/. Make sure that a query like https://keybase.io/_/api/1.0/user/lookup.json?username=polkastats returns your information.</li>
-          <li>5. Make a Pull Request to master branch of the original repo: <a href="https://github.com/Colm3na/polkastats-v2" target="_blank">https://github.com/Colm3na/polkastats-v2</a>.</li>
-          <li>6. That's it! In our next release your validator information shoud appears in PolkaStats :-).</li>
-        </ul>
+        <div class="row">
+          <div class="col-md-4">
+            <h3>1</h3>
+            <p>Go to PolkaStats v2 GitHub repo at <a href="https://github.com/Colm3na/polkastats-v2" target="_blank">https://github.com/Colm3na/polkastats-v2</a></p>
+          </div>
+          <div class="col-md-4">
+            <h3>2</h3>
+            <p>Make a Fork of PolkaStats v2.</p>
+          </div>
+          <div class="col-md-4">
+            <h3>3</h3>
+            <p>Create on it a new file in identities folder named like stash_account_of_your_validator.json. Example: <a href="https://raw.githubusercontent.com/Colm3na/polkastats-v2/master/identities/GTzRQPzkcuynHgkEHhsPBFpKdh4sAacVRsnd8vYfPpTMeEY.json" target="_blank">GTzRQPzkcuynHgkEHhsPBFpKdh4sAacVRsnd8vYfPpTMeEY.json</a></p>
+          </div>
+          <div class="col-md-4">
+            <h3>4</h3>
+            <p>The content of the file should be your username in <a href="https://keybase.io/" target="_blank">https://keybase.io/</a>. Make sure that a query like <a href="https://keybase.io/_/api/1.0/user/lookup.json?username=polkastats" target="_blank">https://keybase.io/_/api/1.0/user/lookup.json?username=polkastats</a> should return the information you want to include.</p>
+          </div>
+          <div class="col-md-4">
+            <h3>5</h3>
+            <p>Make a Pull Request to master branch of the original repo: <a href="https://github.com/Colm3na/polkastats-v2" target="_blank">https://github.com/Colm3na/polkastats-v2</a>.</p>
+          </div>
+          <div class="col-md-4">
+            <h3>6</h3>
+            <p>That's it! In our next release your validator information shoud appears in PolkaStats :-).</p>
+          </div>
+        </div>
 
-        <p>Need help? You can send me a message at <a target="_blank" href="mailto:polkastats@protonmail.com">polkastats@protonmail.com</a>.</p>
+        <h3>Need help? Send me a message at <a target="_blank" href="mailto:polkastats@protonmail.com">polkastats@protonmail.com</a>.</h3>
 
       </b-container>
     </section>
