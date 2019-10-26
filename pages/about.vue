591ccb20f71788d8dc4879442a5b16bd0d3213aa
mariopino <info@quequiereshacer.es>
2019-10-26 05:06:56 +0200

    Update about

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@
     <section>
       <b-container class="main pt-4 pb-5">
         <h1 class="mb-4">ABOUT</h1>
-        <p>This is about <a href="https://polkadot.network" target="_blank"><strong>Polkadot</strong></a>, of course ;-)</p>
+        <p>This is about <a href="https://polkadot.network" target="_blank"><strong>Polkadot</strong></a> and now <a href="https://kusama.network" target="_blank"><strong>Kusama</strong></a>, of course ;-)</p>
         <p>I code this app to experiment and learn about how to retrieve and represent live (and useful for humans :-)) information about the Polkadot network. </p>
         <p>Thanks to all the people from <a href="https://riot.im/app/#/room/#polkadot-watercooler:matrix.org" target="_blank">Polkadot Watercooler</a> for point me to the right direction and save me hours of useless coding.</p>
       
