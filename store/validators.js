65f5a085b925758a1ff9d65d23cd889eab29a359
mariopino <info@quequiereshacer.es>
2019-11-10 01:21:08 +0100

    Add percentage of total stake in validators

--- store/validators.js
+++ store/validators.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import BN from 'bn.js';
+import { isHex } from '@polkadot/util';
 
 export const state = () => ({
-  list: []
+  list: [],
+  totalStakeBonded: 0
 })
   
 export const mutations = {
@@ -11,9 +14,25 @@ export const mutations = {
     this.$ga.event('vuex-store', 'update-validators');
 
     // Sort validators by total bonded desc
-    validators.sort((a, b) => (a.stakers.total < b.stakers.total) ? 1 : -1)
+    validators.sort((a, b) => (a.stakers.total < b.stakers.total) ? 1 : -1);
 
+    // Update validator list
     state.list = validators;
+
+    // Calculate and update active total bonded funds
+    let accum = new BN('0', 10);
+    for (let i = 0; i < validators.length; i++) {
+      let validator = validators[i];
+      let bn;
+      if (isHex(validator.stakers.total)) {
+        bn = new BN(validator.stakers.total.substring(2, validator.stakers.total.length), 16);
+      } else {
+        bn = new BN(validator.stakers.total.toString(), 10);
+      }
+      accum = accum.add(bn);
+    }
+    state.totalStakeBonded = accum;
+    
   },
   getters: function() {
     list: state => state.list
