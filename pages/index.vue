25487298dc2dddd0a47b823dc9a34e1bdaeae343
mariopino <info@quequiereshacer.es>
2019-11-10 21:59:36 +0100

    Fix

--- pages/index.vue
+++ pages/index.vue
@@ -728,7 +728,7 @@ export default {
       return this.$store.state.nicknames.list
     },
     totalStakeBondedPercen() {
-      if (this.totalStakeBonded !== 0 && this.totalIssuance !== 0) {
+      if (this.totalStakeBonded !== 0 || this.totalIssuance !== 0) {
         let totalIssuance = new BN(this.totalIssuance.toString(), 10);
         let totalStakeBonded = this.totalStakeBonded.mul(new BN('100', 10));
         return totalStakeBonded.div(totalIssuance);
