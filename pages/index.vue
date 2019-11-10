c728d00fca337b463ca8f578c548ff1bff52fb04
mariopino <info@quequiereshacer.es>
2019-11-10 23:14:56 +0100

    Fix

--- pages/index.vue
+++ pages/index.vue
@@ -728,12 +728,12 @@ export default {
       return this.$store.state.nicknames.list
     },
     totalStakeBondedPercen() {
-      if (this.totalStakeBonded || this.totalIssuance) {
-        return 0;
-      } else {
-        let totalIssuance = new BN(this.totalIssuance.toString(), 10);
+      if (this.totalStakeBonded !== 0 && this.totalIssuance !== "") {
+        let totalIssuance = new BN(this.totalIssuance, 10);
         let totalStakeBonded = this.totalStakeBonded.mul(new BN('100', 10));
         return totalStakeBonded.div(totalIssuance);
+      } else {
+        return 0;
       }
     }
   },
