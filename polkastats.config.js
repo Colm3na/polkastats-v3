c4e36e116aefbf6aa4ef7e8c000b9d8c7b08c490
mariopino <info@quequiereshacer.es>
2019-11-24 22:10:38 +0100

    Change block explorer

--- polkastats.config.js
+++ polkastats.config.js
@@ -3,6 +3,6 @@ export const unit = `KSM`;
 export const decimals = 12;
 export const backendBaseURL = `https://polkastats.io:8443`;
 export const blockExplorer = {
-  block: `https://polkascan.io/pre/kusama-cc2/block/`,
-  account: `https://polkascan.io/pre/kusama-cc2/account/`
+  block: `https://kusama.colmenalabs.org/block/`,
+  account: `https://kusama.colmenalabs.org/account/`
 };
