3f4f5ee0c9c68b9b49231ee1c27c98d1f45122c2
mariopino <info@quequiereshacer.es>
2019-12-30 23:47:24 +0100

    Show number of accounts

--- pages/accounts.vue
+++ pages/accounts.vue
@@ -6,7 +6,7 @@
 
         <!-- Info message -->
         <b-alert show dismissible variant="success" class="text-center mb-4">
-          All active Kusama accounts, updated every 5 minutes. Click on account to see full details.
+          Showing <strong>{{ accounts.length }} active Kusama accounts</strong>, updated every 5 minutes. Click on account to see full details.
         </b-alert>
 
         <!-- Filter -->
