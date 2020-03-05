49ee465b261d23c8239056311e2ed9896095e218
mariopino <info@quequiereshacer.es>
2020-03-05 11:20:05 +0100

    Change backendBaseURL

--- polkastats.config.js
+++ polkastats.config.js
@@ -1,7 +1,7 @@
 /* PolkaStats app configuration params */
 export const unit = `KSM`;
 export const decimals = 12;
-export const backendBaseURL = `https://polkastats.io/api/`;
+export const backendBaseURL = `https://polkastats.io/api/v2`;
 export const blockExplorer = {
   block: `https://polkascan.io/pre/kusama-cc3/block/`,
   account: `https://polkascan.io/pre/kusama-cc3/account/`
