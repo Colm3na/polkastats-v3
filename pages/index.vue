7de5fb4bc0cfb02b84911850026d8dbc56b33b4d
mariopino <info@quequiereshacer.es>
2020-03-22 20:04:01 +0100

    fix name

--- pages/index.vue
+++ pages/index.vue
@@ -493,7 +493,7 @@ export default {
         },
         {
           key: "accountId",
-          label: "✅ Validators",
+          label: "✅ Validator",
           sortable: true,
           filterByFormatted: true
         },
