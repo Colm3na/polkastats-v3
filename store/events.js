dd082e4aeff76bb26d1a0be965efc6b746ac2bc6
Javi <contacto@scriptonita.com>
2020-03-27 12:39:38 +0100

    Change API from v2 to v3

--- store/events.js
+++ store/events.js
@@ -1,5 +1,4 @@
-import axios from "axios";
-import { backendBaseURL } from "../polkastats.config.js";
+import gql from "graphql-tag";
 
 export const state = () => ({
   list: []
@@ -14,10 +13,31 @@ export const mutations = {
   }
 };
 
+const GET_EVENTS = gql`
+  query intention_staking {
+    event(limit: 5000, order_by: { event_index: desc }) {
+      block_number
+      data
+      event_index
+      method
+      phase
+      section
+    }
+  }
+`;
+
 export const actions = {
   update(context) {
-    axios.get(`${backendBaseURL}/events`).then(function(response) {
-      context.commit("update", response.data);
-    });
+    const client = this.app.apolloProvider.defaultClient;
+    client
+      .query({
+        query: GET_EVENTS
+      })
+      .then(({ data }) => {
+        context.commit("update", data.event);
+      })
+      .catch(error => {
+        console.log("Error fetching Intention table: ", error);
+      });
   }
 };
