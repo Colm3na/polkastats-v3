980a0098645f6b17ee241b1e517e446fb3332505
mariopino <info@quequiereshacer.es>
2020-05-29 22:57:10 +0200

    team page

--- pages/team.vue
+++ pages/team.vue
@@ -1,27 +1,12 @@
 <template>
   <b-container class="pt-4">
     <b-row class="center pt-4">
-      <b-card title="PolkaStats Team">
-        Lorem fistrum de la pradera de la pradera me cago en tus muelas ese
-        hombree pupita benemeritaar. Papaar papaar amatomaa qué dise usteer
-        apetecan por la gloria de mi madre ese que llega. A peich llevame al
-        sircoo quietooor se calle ustée no te digo trigo por no llamarte
-        Rodrigor jarl de la pradera llevame al sircoo. Torpedo te va a hasé
-        pupitaa quietooor va usté muy cargadoo. Al ataquerl al ataquerl me cago
-        en tus muelas amatomaa papaar papaar. Qué dise usteer jarl diodenoo al
-        ataquerl ese que llega la caidita pecador tiene musho peligro se calle
-        ustée mamaar hasta luego Lucas. Fistro a peich hasta luego Lucas papaar
-        papaar. Condemor amatomaa tiene musho peligro tiene musho peligro
-        pecador caballo blanco caballo negroorl pupita a gramenawer amatomaa te
-        voy a borrar el cerito. Diodenoo pecador se calle ustée a wan torpedo
-        tiene musho peligro no te digo trigo por no llamarte Rodrigor. Te voy a
-        borrar el cerito al ataquerl caballo blanco caballo negroorl va usté muy
-        cargadoo apetecan va usté muy cargadoo ese que llega te va a hasé
-        pupitaa. Te voy a borrar el cerito hasta luego Lucas jarl a gramenawer
-        benemeritaar ahorarr ese pedazo de no te digo trigo por no llamarte
-        Rodrigor. Hasta luego Lucas apetecan qué dise usteer a peich tiene musho
-        peligro diodeno la caidita. Al ataquerl se calle ustée de la pradera se
-        calle ustée pecador. Qué dise usteer apetecan pupita torpedo.
+      <b-card title="Team ColmenaLabs">
+        Team ColmenaLabs has a vast expertise in blockchain technologies,
+        conducting Ethereum meetups since earlier 2016. ColmenaLabs is also one
+        of the most active blockchain communities of Spain. Our previous
+        blockchain experience has facilitated our landing in the Proof of Stake
+        era, where we are participating on many projects.
       </b-card>
     </b-row>
     <b-row class="pt-4">
@@ -55,39 +40,35 @@ export default {
       members: [
         {
           name: "Mario",
-          picture: "https://dragonstake.io/img/dragonstake/team/Mario.jpg",
+          picture: "/img/team/Mario.jpg",
           description:
-            "Lorem fistrum no te digo trigo por no llamarte Rodrigor diodenoo te voy a borrar el cerito llevame al sircoo está la cosa muy malar. Va usté muy cargadoo al ataquerl pecador pupita sexuarl ese hombree no puedor diodeno fistro."
+            "Founder & dev lead, full stack developer / Susbtrate lover"
         },
         {
           name: "Fredy",
-          picture: "https://dragonstake.io/img/dragonstake/team/Fredy.jpg",
+          picture: "/img/team/Fredy.jpg",
           description:
-            "Lorem fistrum no te digo trigo por no llamarte Rodrigor diodenoo te voy a borrar el cerito llevame al sircoo está la cosa muy malar. Va usté muy cargadoo al ataquerl pecador pupita sexuarl ese hombree no puedor diodeno fistro."
-        },
-        {
-          name: "Javi",
-          picture: "https://picsum.photos/300/300/?image=41",
-          description:
-            "Lorem fistrum no te digo trigo por no llamarte Rodrigor diodenoo te voy a borrar el cerito llevame al sircoo está la cosa muy malar. Va usté muy cargadoo al ataquerl pecador pupita sexuarl ese hombree no puedor diodeno fistro."
-        },
-        {
-          name: "Diego",
-          picture: "https://picsum.photos/300/300/?image=41",
-          description:
-            "Lorem fistrum no te digo trigo por no llamarte Rodrigor diodenoo te voy a borrar el cerito llevame al sircoo está la cosa muy malar. Va usté muy cargadoo al ataquerl pecador pupita sexuarl ese hombree no puedor diodeno fistro."
+            "DragonStake founder, inflexible product owner and sysadmin guru"
         },
         {
           name: "Wimel",
-          picture: "https://dragonstake.io/img/dragonstake/team/Wimel.png",
-          description:
-            "Lorem fistrum no te digo trigo por no llamarte Rodrigor diodenoo te voy a borrar el cerito llevame al sircoo está la cosa muy malar. Va usté muy cargadoo al ataquerl pecador pupita sexuarl ese hombree no puedor diodeno fistro."
+          picture: "/img/team/Wimel.png",
+          description: "DelegaNetworks founder, i18n and Colony DAO master"
+        },
+        {
+          name: "Scriptonita",
+          picture: "/img/team/Scriptonita.jpg",
+          description: "Frontend developer & javascript/vue/react ninja"
+        },
+        {
+          name: "Diego",
+          picture: "/img/team/Diego.jpg",
+          description: "Black belt javascript/vue developer and docker master"
         },
         {
           name: "Ana",
-          picture: "https://dragonstake.io/img/dragonstake/team/Ana.jpg",
-          description:
-            "Lorem fistrum no te digo trigo por no llamarte Rodrigor diodenoo te voy a borrar el cerito llevame al sircoo está la cosa muy malar. Va usté muy cargadoo al ataquerl pecador pupita sexuarl ese hombree no puedor diodeno fistro."
+          picture: "/img/team/Ana.jpg",
+          description: "Full stack blockchain developer and beloved pr reviewer"
         }
       ]
     };
