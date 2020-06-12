93fa983163239451efdcf21face3f162525a902d
mariopino <info@quequiereshacer.es>
2020-06-12 12:05:40 +0200

    fix

--- mixins/commonMixin.js
+++ mixins/commonMixin.js
@@ -66,7 +66,7 @@ export default {
       return message;
     },
     getStakePercent(amount, totalStakeBonded) {
-      if (amount === 0 || totalStakeBonded === 0) {
+      if (amount === 0 || totalStakeBonded.isEqualTo(new BN("0", 10))) {
         return `0`;
       }
       let amountBN;
