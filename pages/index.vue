935adf9df17289ed015e2ec88017cb2494589d7a
mariopino <info@quequiereshacer.es>
2020-06-29 20:00:33 +0200

    add validator message

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,33 @@
   <div>
     <section>
       <b-container class="main pt-4 pb-5">
-        <h1 class="mb-4">
-          {{ $t("pages.dashboard.title") }}
-        </h1>
+        <b-alert show dismissible variant="primary" class="text-center">
+          <div>
+            <h3>{{ $t("pages.dashboard.polkastats_validator") }}</h3>
+            <h5>{{ $t("pages.dashboard.polkastats_validator_subtitle") }}</h5>
+          </div>
+          <Identicon
+            value="GTzRQPzkcuynHgkEHhsPBFpKdh4sAacVRsnd8vYfPpTMeEY"
+            size="30"
+            theme="polkadot"
+            class="my-3"
+          />
+          <nuxt-link
+            to="/validator?accountId=GTzRQPzkcuynHgkEHhsPBFpKdh4sAacVRsnd8vYfPpTMeEY"
+          >
+            <span
+              v-b-tooltip.hover
+              class="d-inline d-sm-none d-md-none d-lg-none d-xl-none"
+              title="GTzRQPzkcuynHgkEHhsPBFpKdh4sAacVRsnd8vYfPpTMeEY"
+              >{{
+                shortAddress(`GTzRQPzkcuynHgkEHhsPBFpKdh4sAacVRsnd8vYfPpTMeEY`)
+              }}</span
+            >
+            <span class="d-none d-sm-inline d-md-inline d-lg-inline d-xl-inline"
+              >GTzRQPzkcuynHgkEHhsPBFpKdh4sAacVRsnd8vYfPpTMeEY</span
+            >
+          </nuxt-link>
+        </b-alert>
 
         <Chain />
 
