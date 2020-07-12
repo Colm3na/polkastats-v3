6392051a60143acc04e620a8d4c0e9d6c1474674
mariopino <info@quequiereshacer.es>
2020-07-12 20:26:50 +0200

    add networks object

--- polkastats.config.js
+++ polkastats.config.js
@@ -14,3 +14,24 @@ export const numItemsTableOptions = [10, 20, 50, 100];
 // Breakpoints
 export const mobileBreakpoint = 415;
 export const mediumBreakpoint = 765;
+
+export const networks = [
+  {
+    id: "polkadot-cc1",
+    name: "Polkadot",
+    denom: "DOT",
+    decimalPlaces: 12,
+    addressPrefix: 0,
+    nodeWs: "wss://rpc.polkadot.io",
+    backendWs: "wss://polkastats.io/api/v3"
+  },
+  {
+    id: "kusama-cc3",
+    name: "Kusama",
+    denom: "KSM",
+    decimalPlaces: 12,
+    addressPrefix: 2,
+    nodeWs: "wss://kusama-rpc.polkadot.io",
+    backendWs: "wss://kusama.polkastats.io/api/v3"
+  }
+];
